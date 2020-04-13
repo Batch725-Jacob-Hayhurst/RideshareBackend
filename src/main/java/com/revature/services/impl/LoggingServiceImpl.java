9b776b3017e40bf5590b7d0629e19daf12befc3a
MitchellTim29 <mitchelltim29@gmail.com>
2020-04-13 16:30:33 -0400

    completed documentation for advice and logging

--- src/main/java/com/revature/services/impl/LoggingServiceImpl.java
+++ src/main/java/com/revature/services/impl/LoggingServiceImpl.java
@@ -15,8 +15,8 @@ import org.springframework.stereotype.Component;
 import com.revature.services.LoggingService;
 
 /**
- * LoggingServiceImpl handles any additional services that need to be made
- * before calling the repository methods.
+ * LoggingServiceImpl takes in the http response or request along with an object called "body" that corresponds to the respective request or response.
+ * It then logs the request or response.
  * 
  * @author Timothy Mitchell
  *
@@ -26,6 +26,15 @@ import com.revature.services.LoggingService;
 @Slf4j
 public class LoggingServiceImpl implements LoggingService {
     
+	/**
+	 * Takes in the request and request body object from the CustomRequestBodyAdviceAdapter. It the creates a string builder and a map that maps parameters
+	 * of the http request. After this, it appends the request information to the string builder. If the parameter map is empty, it attaches the parameters
+	 * to the string builder. If the body is not null, the body is added to the string builder and then the request is logged.
+	 * 
+	 * @param httpServletRequest represents the http request sent
+	 * @param body represents the extracted object representing the request body
+	 */
+	
     @Override
     public void logRequest(HttpServletRequest httpServletRequest, Object body) {
         StringBuilder stringBuilder = new StringBuilder();
@@ -47,6 +56,16 @@ public class LoggingServiceImpl implements LoggingService {
         log.info(stringBuilder.toString());
     }
     
+    /**
+	 * Takes in the response and response body object from the CustomResponseBodyAdviceAdapter. It the creates a string builder and a map that maps parameters
+	 * of the http response. After this, it appends the response information to the string builder. If the parameter map is empty, it attaches the parameters
+	 * to the string builder. If the body is not null, the body is added to the string builder and then the response is logged.
+	 * 
+	 * @param httpServletRequest represents the http request sent
+	 * @param httpServletResponse represents the http response to be sent
+	 * @param body represents the object of the response body
+	 */
+    
     @Override
     public void logResponse(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object body) {
         StringBuilder stringBuilder = new StringBuilder();
@@ -59,6 +78,13 @@ public class LoggingServiceImpl implements LoggingService {
         
         log.info(stringBuilder.toString());
     }
+    
+    /**
+	 * Creates the map of parameters pertaining to each http request. 
+	 * 
+	 * @param httpServletReqeust represents the http request sent
+	 * @return returns the map of the parameters
+	 */
     
     private Map<String, String> buildParametersMap(HttpServletRequest httpServletRequest) {
         Map<String, String> resultMap = new HashMap<>();
@@ -73,7 +99,15 @@ public class LoggingServiceImpl implements LoggingService {
         return resultMap;
     }
     
-    private Map<String, String> buildHeadersMap(HttpServletRequest request) {
+    /**
+	 * Creates the map of the request headers
+	 * 
+	 * @param request represents the http request sent
+	 * @return returns the map of the headers for the request
+	 */
+    
+    @SuppressWarnings("rawtypes")
+	private Map<String, String> buildHeadersMap(HttpServletRequest request) {
         Map<String, String> map = new HashMap<>();
         
         Enumeration headerNames = request.getHeaderNames();
@@ -85,6 +119,13 @@ public class LoggingServiceImpl implements LoggingService {
         
         return map;
     }
+    
+    /**
+	 * Creates the map of the response headers
+	 * 
+	 * @param response represents the http response to be sent
+	 * @return returns the map of the headers for the response
+	 */
     
     private Map<String, String> buildHeadersMap(HttpServletResponse response) {
         Map<String, String> map = new HashMap<>();
