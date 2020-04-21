c75ce3641e64a573e8036fb6e304f84a72f85a0f
Aaron-Ri <aaron.s.ried@gmail.com>
2020-04-21 10:29:53 -0500

    changes to things and stuff

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,10 +1,10 @@
 insert into batches values
-(1, 'Morgantown, WV 26506'),
-(2, '11730 Plaza America Dr Reston, VA 20190'),
-(3, 'Dallas'),
-(4, 'Tampa'),
-(5, 'New York City'),
-(6, 'Orlando');
+(1, 'Morgantown, WV'),
+(2, 'Reston, VA'),
+(3, 'Dallas, TX'),
+(4, 'Tampa, FL' ),
+(5, 'New York City, NY'),
+(6, 'Orlando, FL');
 
 insert into admins values
 (1, 'admin1');
@@ -12,10 +12,10 @@ insert into admins values
 insert into users values 
 (1, 'gpichmann0@artisteer.com', 'Grady', '609 Legacy Pride Dr', '#348', 'Herndon', 'VA', '20170', false, false, false, 'Pichmann', '212-374-3466', 'gpichmann0', '30401 Esker Point', 'Des Moines', 'IA', '50347', 1),
 (2, 'smigheli1@indiatimes.com', 'Salvidor', '561 Florida Ave', '', 'Herndon', 'VA', '20170', true, true, true, 'Migheli', '614-513-2776', 'smigheli1', '35747 3rd Park', 'Philadelphia', 'PA', '19131', 1),
-(3, 'fbraunroth2@ebay.co.uk', 'Fabien', '12919 Alton Square', '', 'Herndon', 'VA', '20170', true, true, true, 'Braunroth', '402-694-8099', 'fbraunroth2', '747 Crownhardt Place', 'New York City', 'NY', '10270', 1),
-(4, 'aocullen3@google.com.au', 'Aldon', '2456 Masons Ferry Dr', '', 'Herndon', 'VA', '20171', true, false, false, 'O''Cullen', '607-732-5313', 'aocullen3', '2 Killdeer Avenue', 'Norwalk', 'CT', '06859', 1),
-(5, 'hgledhill4@hatena.ne.jp', 'Hilda', '496 High St', '', 'Morgantown', 'WV', '26505', true, true, true, 'Gledhill', '239-528-0279', 'hgledhill4', '45476 Oriole Drive', 'Winston Salem', 'NC', '27110', 1),
-(6, 'sdehoogh5@cnn.com', 'Simonne', '2 West St', '', 'Westover', 'WV', '26501', false, false, true, 'De Hoogh', '206-506-8925', 'sdehoogh5', '68 Almo Terrace', 'New York City', 'NY', '10029', 1),
+(3, 'fbraunroth2@ebay.co.uk', 'Fabien', '12919 Alton Square', '', 'Herndon', 'VA', '20170', true, true, true, 'Braunroth', '402-694-8099', 'fbraunroth2', '747 Crownhardt Place', 'New York City', 'NY', '10270', 3),
+(4, 'aocullen3@google.com.au', 'Aldon', '2456 Masons Ferry Dr', '', 'Herndon', 'VA', '20171', true, false, false, 'O''Cullen', '607-732-5313', 'aocullen3', '2 Killdeer Avenue', 'Norwalk', 'CT', '06859', 4),
+(5, 'hgledhill4@hatena.ne.jp', 'Hilda', '496 High St', '', 'Morgantown', 'WV', '26505', true, true, true, 'Gledhill', '239-528-0279', 'hgledhill4', '45476 Oriole Drive', 'Winston Salem', 'NC', '27110', 5),
+(6, 'sdehoogh5@cnn.com', 'Simonne', '2 West St', '', 'Westover', 'WV', '26501', false, false, true, 'De Hoogh', '206-506-8925', 'sdehoogh5', '68 Almo Terrace', 'New York City', 'NY', '10029', 6),
 (7, 'dosullivan6@loc.gov', 'Darren', '21 Newton Ave', '', 'Morgantown', 'WV', '26505', true, true, true, 'O Sullivan', '614-197-3998', 'dosullivan6', '8445 Novick Hill', 'Gainesville', 'GA', '30506', 1),
 (8, 'ddorro7@issuu.com', 'Donnell', '409 Mclane Ave', '', 'Morgantown', 'WV', '26505', false, true, false, 'Dorro', '559-476-1222', 'ddorro7', '7175 Ruskin Point', 'Santa Monica', 'CA', '90405', 1),
 (9, 'bmacvean8@shop-pro.jp', 'Brynne', '483 Rebecca St', '', 'Morgantown', 'WV', '26505', true, false, false, 'MacVean', '734-143-5188', 'bmacvean8', '39545 Canary Trail', 'Durham', 'NC', '27710', 1),
@@ -26,11 +26,11 @@ insert into users values
 (14, 'flancashired@slideshare.net', 'Faythe', '349 Demain Ave', '', 'Morgantown', 'WV', '26501', true, true, true, 'Lancashire', '216-591-6593', 'flancashired', '1008 Pleasure Terrace', 'San Diego', 'CA', '92176', 1),
 (15, 'llynnitte@creativecommons.org', 'Lorita', '2160 Astoria Cir', '', 'Herndon', 'VA', '20170', true, false, true, 'Lynnitt', '704-705-3599', 'llynnitte', '05579 Clarendon Avenue', 'Los Angeles', 'CA', '90015', 1),
 (16, 'modooghainef@reference.com', 'Marcellina', '13675 Legacy Cir', '', 'Herndon', 'VA', '20171', true, true, false, 'O''Dooghaine', '208-387-2634', 'modooghainef', '27040 Carioca Crossing', 'Detroit', 'MI', '48242', 1),
-(17, 'jsellwoodg@wisc.edu', 'Jaine', '1741 Sundance Dr', '', 'Reston', 'VA', '20194', false, true, false, 'Sellwood', '302-836-7801', 'jsellwoodg', '39 Merry Hill', 'Gadsden', 'AL', '35905', 1),
-(18, 'wviani@homestead.com', 'Wain', '2073 Royal Fern Ct', '', 'Reston', 'VA', '20191', false, false, true, 'Vian', '704-338-2790', 'wviani', '99 Delaware Hill', 'Athens', 'GA', '30605', 1),
-(19, 'gtootellj@latimes.com', 'Geneva', '609 James Cirone Way', '', 'Herndon', 'VA', '20170', false, false, false, 'Tootell', '714-167-0152', 'gtootellj', '61696 Algoma Park', 'Terre Haute', 'IN', '47812', 1),
-(20, 'cgeertzk@senate.gov', 'Cissy', '2482 Masons Ferry Dr', '', 'Herndon', 'VA', '20171', false, false, true, 'Geertz', '281-310-3238', 'cgeertzk', '3 Dexter Alley', 'Frederick', 'MD', '21705', 1),
-(21, 'lozintsevl@freewebs.com', 'Lynn', '1216 Wilshire Dr', '', 'Herndon', 'VA', '20170', true, true, true, 'Ozintsev', '704-108-6483', 'lozintsevl', '38 Muir Plaza', 'Pittsburgh', 'PA', '15210', 1);
+(17, 'jsellwoodg@wisc.edu', 'Jaine', '1741 Sundance Dr', '', 'Reston', 'VA', '20194', false, true, false, 'Sellwood', '302-836-7801', 'jsellwoodg', '39 Merry Hill', 'Gadsden', 'AL', '35905', 2),
+(18, 'wviani@homestead.com', 'Wain', '2073 Royal Fern Ct', '', 'Reston', 'VA', '20191', false, false, true, 'Vian', '704-338-2790', 'wviani', '99 Delaware Hill', 'Athens', 'GA', '30605', 2),
+(19, 'gtootellj@latimes.com', 'Geneva', '609 James Cirone Way', '', 'Herndon', 'VA', '20170', false, false, false, 'Tootell', '714-167-0152', 'gtootellj', '61696 Algoma Park', 'Terre Haute', 'IN', '47812', 2),
+(20, 'cgeertzk@senate.gov', 'Cissy', '2482 Masons Ferry Dr', '', 'Herndon', 'VA', '20171', false, false, true, 'Geertz', '281-310-3238', 'cgeertzk', '3 Dexter Alley', 'Frederick', 'MD', '21705', 2),
+(21, 'lozintsevl@freewebs.com', 'Lynn', '1216 Wilshire Dr', '', 'Herndon', 'VA', '20170', true, true, true, 'Ozintsev', '704-108-6483', 'lozintsevl', '38 Muir Plaza', 'Pittsburgh', 'PA', '15210', 2);
 
 
 insert into cars values 
