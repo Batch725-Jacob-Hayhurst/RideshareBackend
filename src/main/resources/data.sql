899fce385393cb4a8e212e43c6ff2abc4625810e
EricTBKelley <ericthomaskelley@gmail.com>
2020-04-21 15:24:35 -0500

    sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -9,47 +9,59 @@ insert into batches values
 insert into admins values
 (1, 'admin1');
 
+--Non Drivers
 insert into users values 
 (1, 'gpichmann0@artisteer.com', 'Grady', '609 Legacy Pride Dr', '#348', 'Herndon', 'VA', '20170', false, false, false, 'Pichmann', '212-374-3466', 'gpichmann0', '30401 Esker Point', 'Des Moines', 'IA', '50347', 1),
-(2, 'smigheli1@indiatimes.com', 'Salvidor', '561 Florida Ave', '', 'Herndon', 'VA', '20170', true, true, true, 'Migheli', '614-513-2776', 'smigheli1', '35747 3rd Park', 'Philadelphia', 'PA', '19131', 1),
 (4, 'aocullen3@google.com.au', 'Aldon', '2456 Masons Ferry Dr', '', 'Herndon', 'VA', '20171', true, false, false, 'O''Cullen', '607-732-5313', 'aocullen3', '2 Killdeer Avenue', 'Norwalk', 'CT', '06859', 4),
-(5, 'hgledhill4@hatena.ne.jp', 'Hilda', '496 High St', '', 'Morgantown', 'WV', '26505', true, true, true, 'Gledhill', '239-528-0279', 'hgledhill4', '45476 Oriole Drive', 'Winston Salem', 'NC', '27110', 5),
 (6, 'sdehoogh5@cnn.com', 'Simonne', '2 West St', '', 'Westover', 'WV', '26501', false, false, true, 'De Hoogh', '206-506-8925', 'sdehoogh5', '68 Almo Terrace', 'New York City', 'NY', '10029', 6),
-(7, 'dosullivan6@loc.gov', 'Darren', '21 Newton Ave', '', 'Morgantown', 'WV', '26505', true, true, true, 'O Sullivan', '614-197-3998', 'dosullivan6', '8445 Novick Hill', 'Gainesville', 'GA', '30506', 1),
 (8, 'ddorro7@issuu.com', 'Donnell', '409 Mclane Ave', '', 'Morgantown', 'WV', '26505', false, true, false, 'Dorro', '559-476-1222', 'ddorro7', '7175 Ruskin Point', 'Santa Monica', 'CA', '90405', 1),
 (9, 'bmacvean8@shop-pro.jp', 'Brynne', '483 Rebecca St', '', 'Morgantown', 'WV', '26505', true, false, false, 'MacVean', '734-143-5188', 'bmacvean8', '39545 Canary Trail', 'Durham', 'NC', '27710', 1),
-(10, 'tcurd9@illinois.edu', 'Tobe', '5206 Teaberry Ln', '', 'Morgantown', 'WV', '26508', true, true, true, 'Curd', '646-919-8954', 'tcurd9', '127 Dunning Plaza', 'Saint Louis', 'MO', '63131', 1),
-(11, 'medmundsa@tiny.cc', 'Marilyn', '357 patteson Drive', '', 'Morgantown', 'WV', '26505', true, true, true, 'Edmunds', '213-600-8430', 'medmundsa', '6 Bay Road', 'Columbia', 'MO', '65218', 1),
 (12, 'wmatissoffb@facebook.com', 'Wynne', '815 Fenwick Ave', '', 'Morgantown', 'WV', '26505', false, false, true, 'Matissoff', '405-888-7795', 'wmatissoffb', '1 Lakeland Way', 'Shawnee Mission', 'KS', '66215', 1),
 (13, 'jlawfulc@simplemachines.org', 'Jozef', '837 Naomi St', '', 'Morgantown', 'WV', '26505', false, true, true, 'Lawful', '509-805-2290', 'jlawfulc', '492 Quincy Road', 'Houston', 'TX', '77035', 1),
-(14, 'flancashired@slideshare.net', 'Faythe', '349 Demain Ave', '', 'Morgantown', 'WV', '26501', true, true, true, 'Lancashire', '216-591-6593', 'flancashired', '1008 Pleasure Terrace', 'San Diego', 'CA', '92176', 1),
 (15, 'llynnitte@creativecommons.org', 'Lorita', '2160 Astoria Cir', '', 'Herndon', 'VA', '20170', true, false, true, 'Lynnitt', '704-705-3599', 'llynnitte', '05579 Clarendon Avenue', 'Los Angeles', 'CA', '90015', 1),
 (16, 'modooghainef@reference.com', 'Marcellina', '13675 Legacy Cir', '', 'Herndon', 'VA', '20171', true, true, false, 'O''Dooghaine', '208-387-2634', 'modooghainef', '27040 Carioca Crossing', 'Detroit', 'MI', '48242', 1),
 (17, 'jsellwoodg@wisc.edu', 'Jaine', '1741 Sundance Dr', '', 'Reston', 'VA', '20194', false, true, false, 'Sellwood', '302-836-7801', 'jsellwoodg', '39 Merry Hill', 'Gadsden', 'AL', '35905', 2),
 (18, 'wviani@homestead.com', 'Wain', '2073 Royal Fern Ct', '', 'Reston', 'VA', '20191', false, false, true, 'Vian', '704-338-2790', 'wviani', '99 Delaware Hill', 'Athens', 'GA', '30605', 2),
 (19, 'gtootellj@latimes.com', 'Geneva', '609 James Cirone Way', '', 'Herndon', 'VA', '20170', false, false, false, 'Tootell', '714-167-0152', 'gtootellj', '61696 Algoma Park', 'Terre Haute', 'IN', '47812', 2),
-(20, 'cgeertzk@senate.gov', 'Cissy', '2482 Masons Ferry Dr', '', 'Herndon', 'VA', '20171', false, false, true, 'Geertz', '281-310-3238', 'cgeertzk', '3 Dexter Alley', 'Frederick', 'MD', '21705', 2),
+(20, 'cgeertzk@senate.gov', 'Cissy', '2482 Masons Ferry Dr', '', 'Herndon', 'VA', '20171', false, false, true, 'Geertz', '281-310-3238', 'cgeertzk', '3 Dexter Alley', 'Frederick', 'MD', '21705', 2);
+
+--Morgan Town Drivers [5]
+insert into users values
+(2, 'smigheli1@indiatimes.com', 'Salvidor', '561 Florida Ave', '', 'Herndon', 'VA', '20170', true, true, true, 'Migheli', '614-513-2776', 'smigheli1', '35747 3rd Park', 'Philadelphia', 'PA', '19131', 1),
+(7, 'dosullivan6@loc.gov', 'Darren', '21 Newton Ave', '', 'Morgantown', 'WV', '26505', true, true, true, 'O Sullivan', '614-197-3998', 'dosullivan6', '8445 Novick Hill', 'Gainesville', 'GA', '30506', 1),
+(10, 'tcurd9@illinois.edu', 'Tobe', '5206 Teaberry Ln', '', 'Morgantown', 'WV', '26508', true, true, true, 'Curd', '646-919-8954', 'tcurd9', '127 Dunning Plaza', 'Saint Louis', 'MO', '63131', 1),
+(11, 'medmundsa@tiny.cc', 'Marilyn', '357 patteson Drive', '', 'Morgantown', 'WV', '26505', true, true, true, 'Edmunds', '213-600-8430', 'medmundsa', '6 Bay Road', 'Columbia', 'MO', '65218', 1),
+(14, 'flancashired@slideshare.net', 'Faythe', '349 Demain Ave', '', 'Morgantown', 'WV', '26501', true, true, true, 'Lancashire', '216-591-6593', 'flancashired', '1008 Pleasure Terrace', 'San Diego', 'CA', '92176', 1);
+
+--Reston Drivers [4]
+insert into users values
 (21, 'lozintsevl@freewebs.com', 'Lynn', '1216 Wilshire Dr', '', 'Herndon', 'VA', '20170', true, true, true, 'Ozintsev', '704-108-6483', 'lozintsevl', '38 Muir Plaza', 'Pittsburgh', 'PA', '15210', 2),
 (23, 'kfed@kpop.com', 'Kyle', '1400 Lake Fairfax Dr', '', 'Reston', 'VA', '20190', true, true, true, 'Fields', '703-203-6835', 'KFED', '11730 Plaza America Dr. 2nd Floor', 'Reston', 'VA', '20190', 2), 
 (24, 'ManDude@DudeMan.net', 'Dude', '11730 Plaza America Dr 2nd Floor', '', 'Reston', 'VA', '20190', true, true, true, 'Man', '893-444-3245', 'ManDude', '1301 S Crossbow Pl', 'Chandler', 'AZ', '85286', 2),
 (25, 'peppercorn@gmail.com', 'Danny', '11710 America Drive', '', 'Reston', 'VA', '20190', true, true, true, 'DeVito', '666-777-9999', 'magnumdriver', '2101 Raven Court', 'Herndon', 'VA', '20170' , 2);
 
+--Dallas Drivers [4]
 insert into users values
 (3, 'fbraunroth2@ebay.co.uk', 'Fabien', '12919 Alton Square', '', 'Herndon', 'VA', '20170', true, true, true, 'Braunroth', '402-694-8099', 'fbraunroth2', '747 Crownhardt Place', 'New York City', 'NY', '10270', 3),
 (22, 'tex@country.web', 'Jim', '701 S Nedderman Drive', '', 'Arlington', 'TX', '76019', true, true, true, 'Dangle', '704-124-5678', 'BigJim', '230 N Center St', 'Arlington', 'TX', '76011', 3),
-(26, 'deebo@morganstate.com', 'Deebo', '702 S Nedderman Drive', '', 'Arlington', 'TX', '76019', true, true, true, 'Diango', '214-009-9007', 'Diango99', '3125 Woodbridge Dr', 'Bedford', 'TX', '76021', 3),
-(27, 'bigtex@county.com', 'Jeremy','703 S Nedderman Drive', '', 'Arlington', 'TX','76019', true, true, true, 'Worthle', '869-459-2263', 'JWorth9', '12 East Kirkland Drive', 'Dallas', 'TX','75224', 3);
+(26, 'deebo@morganstate.com', 'Deebo', '703 S Nedderman Drive', '', 'Arlington', 'TX', '76019', true, true, true, 'Diango', '214-009-9007', 'Diango99', '3125 Woodbridge Dr', 'Bedford', 'TX', '76021', 3),
+(27, 'bigtex@county.com', 'Jeremy','12 East Kirkland Drive', '', 'Dallas', 'TX','75224', true, true, true, 'Worthle', '869-459-2263', 'JWorth9', '12 East Kirkland Drive', 'Dallas', 'TX','75224', 3);
 
+--Tampa Drivers [1]
 insert into users values
-(28, 'dudeman@sir.com', 'Kenny', '4202 E. Fowler Avenue', '', 'Tampa', 'FL', '33620', true, true, true, 'brunsworth', '763-333-3573', 'KennyMan', '3404 W Marlin Ave #123', 'Tampa', 'FL', '33611', 4),
+(28, 'dudeman@sir.com', 'Kenny', '4202 E. Fowler Avenue', '', 'Tampa', 'FL', '33620', true, true, true, 'brunsworth', '763-333-3573', 'KennyMan', '3404 W Marlin Ave #123', 'Tampa', 'FL', '33611', 4);
+
+--New York Drivers [3]
+insert into users values
+(5, 'hgledhill4@hatena.ne.jp', 'Hilda', '496 High St', '', 'Morgantown', 'WV', '26505', true, true, true, 'Gledhill', '239-528-0279', 'hgledhill4', '45476 Oriole Drive', 'Winston Salem', 'NC', '27110', 5),
 (29, 'cxf@sbu.com', 'Chengxi', '65-30 Kissena Blvd. | Queens', '', 'New York City', 'NY', '11367', true, true, true, 'Fan', '718-456-9156', 'cxfan97', '7488 Wilson St', 'Lindenhurst', 'NY', '11757', 5),
-(30, 'liammconverse@gmail.com', 'Liam', '4 Pennsylvania Plaza', '', 'New York City', 'NY', '10001', true, true, true, 'Converse', '703-568-4597', 'LiamC', '36 Kline Blvd', 'Whitehouse', 'NJ', '08889', 5),
+(30, 'liammconverse@gmail.com', 'Liam', '4 Pennsylvania Plaza', '', 'New York City', 'NY', '10001', true, true, true, 'Converse', '703-568-4597', 'LiamC', '36 Kline Blvd', 'Whitehouse', 'NJ', '08889', 5);
+
+--Orlando Drivers [2]
+insert into users values
 (31, 'monty@python.com', 'Monty', '6200 Metrowest Blvd Suite 208', '', 'Orlando', 'FL', '32835', true, true, true, 'Python', '123-123-1234', 'mpython', '15856 Freedom Lane', 'Apple Valley', 'MN', '55124', 6),
 (32, 'jimmy40@revature.com', 'Jiminy', '6201 Metrowest Blvd Suite 208', '', 'Orlando', 'FL', '32835', true, true, true, 'Cricket', '407-321-1940', 'LilJim', '223 E Concord St', 'Orlando', 'FL', '32801', 6);
 
-
---230 N Center St, Arlington, TX 76011
--- Reston, Dallas, Tampa, New York, Orlando
 
 
 insert into cars values 
